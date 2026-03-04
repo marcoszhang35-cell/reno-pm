80ed3e00d6470d8fe59acbfd58c94dc1ef9f598d
Marcos <marcos@MacBook-Pro.local>
2026-03-04 17:55:06 +1300

    reno pm system

---
+++ config/config/options.ts
@@ -0,0 +1,19 @@
+export const SOURCE_OPTIONS = [
+  "Xiaohongshu",
+  "Google",
+  "Facebook",
+  "Referral",
+  "Repeat Client",
+  "Walk-in",
+  "Other",
+] as const;
+
+export const TARGET_AREA_OPTIONS = [
+  "Kitchen",
+  "Bathroom",
+  "Flooring",
+  "Carpet",
+  "Painting",
+  "Full Renovation",
+  "Other",
+] as const;
